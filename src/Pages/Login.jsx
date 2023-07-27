1fae23a6cf55d7dc94e66182fc79c31dc8c0160d
vigneshdevelopr <vigneshtpr99@gmail.com>
2023-07-27 11:00:15 +0530

    Login and Register Implemented

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,118 +1,130 @@
-import React from 'react'
-import { styled } from 'styled-components'
-import BrandLogo from '../assets/brandlogo.png'
-import { useHistory } from 'react-router-dom/cjs/react-router-dom'
+import React from "react";
+import { styled } from "styled-components";
+import BrandLogo from "../assets/brandlogo.png";
+import { useHistory } from "react-router-dom/cjs/react-router-dom";
 
 function Login() {
-    const history = useHistory();
+  const history = useHistory();
   return (
-<Container>
-    <ArcArea>
-
-    <Logo>
-    <img className='logo' src={BrandLogo} alt='Brand Logo' />
-</Logo>
-<Form>
-  <input type="email" placeholder='username' />
-  <input type="password" placeholder='password' />
-   <button>Login</button>
-</Form>
-<h5 style={{textAlign:'center'}}>Don't Have an account ?
+    <Container>
+      <ArcArea>
+        <Logo>
+          <img className="logo" src={BrandLogo} alt="Brand Logo" />
+        </Logo>
+        <Form>
+          <input
+            defaultValue="vigneshwebdevelopr@gmail.com"
+            type="email"
+            placeholder="username"
+          />
+          <input
+            defaultValue="password@123"
+            type="password"
+            placeholder="password"
+          />
+          <button><span style={{fontSize:'larger',fontWeight:'bold', color: '#252525'}}>Login</span></button>
+<h5 style={{textAlign:'center'}}>          By continuing, you agree to TownBazzar's Conditions of Use and Privacy Notice.
 </h5>
-<p onClick={()=>history.push('/register')}>
-    create a new account
-</p>
-    </ArcArea>
-
-</Container>
-    )
+        </Form>
+        <h5 style={{ textAlign: "center" }}>Don't Have an account ?</h5>
+        <p className="createnewaccount" onClick={() => history.push("/register")}>create a new account</p>
+      </ArcArea>
+    </Container>
+  );
 }
 
-export default Login
+export default Login;
 
 const Container = styled.div`
-width: 100%;   
-height: 100vh;
-font-family: 'Marcellus', serif;
-font-size: larger;
-color: whitesmoke;
-/* border: 2px solid black; */
-display: flex;
-justify-content: center;
+  width: 100%;
+  height: 100vh;
+  font-family: "Marcellus", serif;
+  font-size: larger;
+  color: whitesmoke;
+  /* border: 2px solid black; */
+  display: flex;
+  justify-content: center;
 
-background: url('https://images.unsplash.com/photo-1607082348824-0a96f2a4b9da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8c2hvcHBpbmd8ZW58MHx8MHx8fDA%3D&w=1000&q=80');
-background-repeat: no-repeat;
-background-size: cover;
-`
+  background: url("https://images.unsplash.com/photo-1607082348824-0a96f2a4b9da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8c2hvcHBpbmd8ZW58MHx8MHx8fDA%3D&w=1000&q=80");
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
 const Logo = styled.div`
-padding: 20px;
-/* border: 2px solid darkgray; */
-text-align: center;
+  padding: 20px;
+  /* border: 2px solid darkgray; */
+  text-align: center;
 
-
-img{
+  img {
     height: 90px;
-background-color: #e76b6b;
-border-radius:9px;
+    background-color: #e76b6b;
+    border-radius: 9px;
     /* border: 2px solid darkcyan; */
-
-}
-`
+  }
+`;
 const Form = styled.div`
-display: flex;
-flex-direction: column;
-width: fit-content;
-margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  width: fit-content;
+  margin: 0 auto;
 
-
-input{
+  input {
     display: flex;
     margin-top: 10px;
-    border: none;
     padding: 10px;
+    border-color: transparent;
     border-radius: 8px;
-
-}
-button{
+    &:hover{
+border-color: #F7CA00;
+    }
+  }
+  button {
     margin-top: 30px !important;
     padding: 10px 30px !important;
-    background-color: mediumseagreen;
-border: none;
-border-radius: 9px;
-width: fit-content;
-display: flex;
-margin: 0 auto;
-padding: 8px;
-    &:hover{
-        cursor: pointer;
-        background-color: #215d3c;
-
+    background-color: #F7CA00;
+    border: none;
+    border-radius: 9px;
+    width: fit-content;
+    display: flex;
+    margin: 0 auto;
+    padding: 8px;
+    &:hover {
+      cursor: pointer;
+      background-color: #e9da9a;
     }
-}
-
-`
+  }
+`;
 const ArcArea = styled.div`
-/* border: 2px solid white; */
-width: fit-content;
-display: flex;
-flex-direction: column;
-padding: 2cm;
-justify-content: center;
-margin: 20px 0;
-border-radius: 10px;
-background-color: #252525;
-opacity: 85%;
+  /* border: 2px solid white; */
+  width: 8cm;
+  display: flex;
+  flex-direction: column;
+  padding: 2cm;
+  justify-content: center;
+  margin: 20px 0;
+  border-radius: 10px;
+  background-color: black;
+  opacity: 80%;
 
-p{
+  p {
     text-align: center;
     color: whitesmoke;
     text-decoration: none;
     margin: 10px;
 
-    &:hover{
-        cursor: pointer;
+    &:hover {
+      cursor: pointer;
     }
+  }
 
-}
-
-`
+  .createnewaccount{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /* border: 1px solid white; */
+    background-color: #F7CA00;
+    color: #252525;
+    border-radius: 9px;
+    height:3rem;
+    
+  }
+`;
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,9 +1,132 @@
-import React from 'react'
+import React from "react";
+import { styled } from "styled-components";
+import BrandLogo from "../assets/brandlogo.png";
+import { useHistory } from "react-router-dom/cjs/react-router-dom";
 
 function Register() {
+  const history = useHistory();
   return (
-    <div>Register</div>
-  )
+    <Container>
+      <ArcArea>
+        <Logo>
+          <img className="logo" src={BrandLogo} alt="Brand Logo" />
+        </Logo>
+        <Form>
+          <input
+            type="email"
+            placeholder="username"
+          />
+          <input
+            type="password"
+            placeholder="password"
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+          />
+          <button><span style={{fontSize:'larger',fontWeight:'bold', color: '#252525'}}>Register</span></button>
+<h5 style={{textAlign:'center'}}>          By continuing, you agree to TownBazzar's Conditions of Use and Privacy Notice.
+</h5>
+        </Form>
+        <h5 style={{ textAlign: "center" }}>Already Have an Account ?</h5>
+        <p className="createnewaccount" onClick={() => history.push("/")}>Login</p>
+      </ArcArea>
+    </Container>
+  );
 }
 
-export default Register
+export default Register;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  font-family: "Marcellus", serif;
+  font-size: larger;
+  color: whitesmoke;
+  /* border: 2px solid black; */
+  display: flex;
+  justify-content: center;
+
+  background: url("https://images.unsplash.com/photo-1607082348824-0a96f2a4b9da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8c2hvcHBpbmd8ZW58MHx8MHx8fDA%3D&w=1000&q=80");
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+const Logo = styled.div`
+  padding: 20px;
+  /* border: 2px solid darkgray; */
+  text-align: center;
+
+  img {
+    height: 90px;
+    background-color: #e76b6b;
+    border-radius: 9px;
+    /* border: 2px solid darkcyan; */
+  }
+`;
+const Form = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: fit-content;
+  margin: 0 auto;
+
+  input {
+    display: flex;
+    margin-top: 10px;
+    padding: 10px;
+    border-color: transparent;
+    border-radius: 8px;
+    &:hover{
+border-color: #F7CA00;
+    }
+  }
+  button {
+    margin-top: 30px !important;
+    padding: 10px 30px !important;
+    background-color: #F7CA00;
+    border: none;
+    border-radius: 9px;
+    width: fit-content;
+    display: flex;
+    margin: 0 auto;
+    padding: 8px;
+    &:hover {
+      cursor: pointer;
+      background-color: #e9da9a;
+    }
+  }
+`;
+const ArcArea = styled.div`
+  /* border: 2px solid white; */
+  width: 8cm;
+  display: flex;
+  flex-direction: column;
+  padding: 2cm;
+  justify-content: center;
+  margin: 20px 0;
+  border-radius: 10px;
+  background-color: black;
+  opacity: 80%;
+
+  p {
+    text-align: center;
+    color: whitesmoke;
+    text-decoration: none;
+    margin: 10px;
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
+
+  .createnewaccount{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    /* border: 1px solid white; */
+    background-color: #F7CA00;
+    color: #252525;
+    border-radius: 9px;
+    height:3rem;
+    
+  }
+`;
